f714b2d278b2e66b23e72b9f929ff0254744beeb
dario <dario.britos@gmail.com>
2017-07-12 00:16:36 -0300

    Commit inicial

---
+++ src/main/java/org/proygrad/sherlock/app/Application.java
@@ -0,0 +1,18 @@
+package org.proygrad.sherlock.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by Dario on 02/07/2017.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+}
